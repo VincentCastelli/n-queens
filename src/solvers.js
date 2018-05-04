34f03a3021eb1cc3afc285df6844efcb16d22813
VincentCastelli <vincentsalvatorecastelli@gmail.com>
2018-05-03 17:26:15 -0700

    Passed test for findNRooksSolution

--- src/solvers.js
+++ src/solvers.js
@@ -13,13 +13,41 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
-
-
+var makeEmptyMatrix = function(n) {
+  return _(_.range(n)).map(function() {
+    return _(_.range(n)).map(function() {
+      return 0;
+    });
+  });
+};
+      
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  //debugger;
+  //var matrix = makeEmptyMatrix(n);
+  var board = new Board({n: n});
+  //board.togglePiece(0, 0);
+  var count = 0;
+  var currentCol = 0;
+  var currentRow = 0;
+  //var matrix = makeEmptyMatrix(n);
+  while (count < n) {
+    board.togglePiece(currentRow, currentCol);
+    currentCol++;
+    currentRow++;
+    count++;
+    if (currentCol >= n) {
+      currentCol = 0;
+    }
+    if (currentRow >= n) {
+      currentRow = 0;
+    }
+  }
+  var matrix = [];
+  for (var i = 0; i < n; i++) {
+    matrix.push(board.get(i));
+  }
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(matrix));
+  return matrix; 
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
